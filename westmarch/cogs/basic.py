f8080d9ea75f6ae5114d4e8b2d503bdb133a5357
Gregory Rome <gwrome@gmail.com>
2020-05-09 12:45:45 -0500

    +class and profession lists, cleaned up help text

--- westmarch/cogs/basic.py
+++ westmarch/cogs/basic.py
@@ -1,53 +1,49 @@
 from discord.ext import commands
 import dice
 import discord
+from westmarch.db.models import CharacterClasses, Professions
 
 
 class Basic(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command()
-    async def hello(self, ctx, member: discord.Member = None):
-        """
-        NI HAO
-        """
-        member = ctx.author
-        await ctx.send(f"NI HAO {member}")
+        self.session = bot.session
 
     @commands.command()
     async def ping(self, ctx):
         """
-        Another test command
+        Ping the guild master
         """
-        pinger = ctx.author
-        bot_name = self.bot.user.name
-        await ctx.send(f"{pinger} sent a ping to {bot_name}")
+        await ctx.send("PONG!")
 
-    @commands.command()
-    async def engineurl(self, ctx):
-        """
-        Test that db is alive
-        """
-        pinger = ctx.author
-        await ctx.send(f"{pinger}, the url for the db is {self.bot.engine.url}")
-
-    @commands.command()
-    async def listtables(self, ctx):
-        """
-        What tables are in the db
-        """
-        pinger = ctx.author
-        print(self.bot.engine.table_names())
-        table_names = ", ".join(self.bot.engine.table_names())
-        await ctx.send(f"{pinger}, the tables in the db are {table_names}")
-
-    @commands.command(
-        name="roll", help="Roll dice, e.g. 3d6+5", aliases=["r", "Roll", "R"]
-    )
+    @commands.command(name="roll", help="!roll 3d6+5", aliases=["r", "Roll", "R"])
     async def roll(self, ctx, roll_string: str):
         try:
             roll_result = str(dice.roll(roll_string))
         except dice.DiceException:
             roll_result = "Invalid roll. Try again."
         await ctx.send(roll_result)
+
+    @commands.command()
+    async def list_classes(self, ctx):
+        """
+        List available classes
+        """
+        classes = [
+            "* " + _.name
+            for _ in self.session.query(CharacterClasses)
+            .order_by(CharacterClasses.name)
+            .all()
+        ]
+        await ctx.send("\n".join(classes))
+
+    @commands.command()
+    async def list_professions(self, ctx):
+        """
+        List available professions
+        """
+        professions = [
+            "* " + _.name
+            for _ in self.session.query(Professions).order_by(Professions.name).all()
+        ]
+        await ctx.send("\n".join(professions))
